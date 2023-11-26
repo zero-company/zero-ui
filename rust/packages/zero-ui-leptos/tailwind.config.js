edbc0d2a9b8fed1aed52dc2e400dd89faa2c1c3c
Jigz <jigzpalillo@gmail.com>
2023-11-26 19:00:33 +0800

    [zero-ui-leptos] Initialize leptos

---
+++ rust/packages/zero-ui-leptos/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    files: ['*.html', '*.css', './src/**/*.rs'],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
