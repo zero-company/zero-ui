4ebb870ee5ea474577766ade08247d888953d7ec
Jigz <jigzpalillo@gmail.com>
2023-12-07 22:36:13 +0800

    [rust] Update build-zero-ui-leptos

--- rust/packages/zero-ui-leptos/src/bin/unpack-zero-ui-assets.rs
+++ rust/packages/zero-ui-leptos/src/bin/unpack-zero-ui-assets.rs
@@ -1,5 +1,5 @@
 pub fn main() {
     let root_dir: std::path::PathBuf = std::env::var("CARGO_MANIFEST_DIR").unwrap().into();
-    let _ = zero_ui::unpack_assets(root_dir.join("assets/zero-ui-assets"));
+    let _ = zero_ui::unpack_assets(root_dir.join("src/assets/zero-ui-assets"));
 }
 
