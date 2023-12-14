6316a8ca76e728640b81551fc6a16dfda9415063
Jigz <jigzpalillo@gmail.com>
2023-12-15 05:40:03 +0800

    [zero-ui-leptos] Update app-container

--- rust/packages/zero-ui-leptos/src/lib/app-container.rs
+++ rust/packages/zero-ui-leptos/src/lib/app-container.rs
@@ -3,7 +3,10 @@ use leptos::*;
 #[component]
 pub fn AppContainer(children: Children) -> impl IntoView {
     view! {
-        <div id="AppContainer" class="flex bg-zinc-900 h-screen w-screen text-zinc-400 text-xs">
+        <div
+            id="AppContainer"
+            class="flex h-screen w-screen overflow-clip font-notoSans bg-zinc-900 text-zinc-400 text-xs"
+        >
             {children()}
         </div>
     }
