6c5e76d987fb1ef75fb443753720763ee3ab2f13
Jigz <jigzpalillo@gmail.com>
2023-12-17 08:23:29 +0800

    [zero-ui] Update Sidebar

--- rust/packages/zero-ui/src/lib/sidebar.rs
+++ rust/packages/zero-ui/src/lib/sidebar.rs
@@ -1,4 +1,6 @@
+use crate::prelude::Icon;
 use leptos::*;
+use leptos_icons::*;
 
 #[component]
 pub fn Sidebar<T, TIV, B, BIV>(
@@ -23,7 +25,25 @@ where
 
     view! {
         <div id=id class=class style=style>
+            <div class="h-9 w-9 text-xl flex justify-center items-center">
+                <svg
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 320 320"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M160 320C248.366 320 320 248.366 320 160C320 71.6344 248.366 0 160 0C71.6344 0 0 71.6344 0 160C0 248.366 71.6344 320 160 320ZM160 272C221.856 272 272 221.856 272 160C272 98.1441 221.856 48 160 48C98.1441 48 48 98.1441 48 160C48 221.856 98.1441 272 160 272Z"
+                        fill="currentColor"
+                    ></path>
+                </svg>
+            </div>
+            <Icon icon=FiIcon::FiSearch/>
             {top_icons()}
+            {bottom_icons()}
         </div>
     }
 }
