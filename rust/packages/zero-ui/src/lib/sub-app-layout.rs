7241b9ec9e457ec756933e496e4dd435899defcc
Jigz <jigzpalillo@gmail.com>
2024-01-02 02:02:25 +0800

    [zero-ui] Update SubAppLayout

--- rust/packages/zero-ui/src/lib/sub-app-layout.rs
+++ rust/packages/zero-ui/src/lib/sub-app-layout.rs
@@ -2,10 +2,21 @@ use leptos::*;
 
 /// SubAppLayout Leptos Component
 #[component]
-pub fn SubAppLayout(children: Children) -> impl IntoView {
+pub fn SubAppLayout<B, BIV, S, SIV>(body: B, sidebar: S) -> impl IntoView
+where
+    B: Fn() -> BIV,
+    BIV: IntoView,
+    S: Fn() -> SIV,
+    SIV: IntoView,
+{
     view! {
-        <div id="SubAppLayout" class="">
-            {children()}
+        <div id="SubAppLayout" class="flex h-screen w-screen zero-divide-x">
+            <div id="SidebarContainer" class="flex h-screen">
+                {sidebar()}
+            </div>
+            <div id="BodyContainer" class="flex flex-1">
+                {body()}
+            </div>
         </div>
     }
 }
