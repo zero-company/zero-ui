3e88def7413e5563ba50b319d7d4342df2d70aa4
Jigz <jigzpalillo@gmail.com>
2024-01-05 11:56:06 +0800

    [zero-ui-leptos] Add apps

---
+++ rust/packages/zero-ui-leptos/src/apps/ui/pages/index-page.rs
@@ -0,0 +1,12 @@
+use leptos::*;
+
+#[component]
+pub fn IndexPage() -> impl IntoView {
+    view! {
+        <div class="my-0 mx-auto max-w-3xl text-center">
+            <h2 class="p-6 text-4xl">"UI Index"</h2>
+
+        </div>
+    }
+}
+
