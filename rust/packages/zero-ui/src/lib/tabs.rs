49b18a0193685fe2b977afb08a206e2cc607dfd8
Jigz <jigzpalillo@gmail.com>
2024-01-17 06:18:58 +0800

    [zero-ui] Update Tabs

--- rust/packages/zero-ui/src/lib/tabs.rs
+++ rust/packages/zero-ui/src/lib/tabs.rs
@@ -8,71 +8,27 @@ pub struct Tab {
 
 /// Tabs Leptos Component
 #[component]
-pub fn Tabs<
-    IV,
-    //TIV
->(
+pub fn Tabs(
     #[prop()] tabs: Vec<Tab>,
-    #[prop(into)] tabs2: Vec<IV>,
     #[prop(optional)] active_tab: Option<ReadSignal<String>>,
-) -> impl IntoView
-where
-    IV: IntoView,
-    //TIV: IntoView,
-{
+) -> impl IntoView {
     view! {
         <div id="Tabs" class="">
-            <p>
-                "active_tab2:"
-                {move || {
-                    match active_tab {
-                        Some(active_tab) => active_tab(),
-                        None => "N/A".to_string(),
-                    }
-                }}
-
-            </p>
-            <p>
-                {move || {
-                    active_tab.map(|active_tab| active_tab.get()).unwrap_or_default()
-                        == "Tab1".to_string()
-                }}
-
-            </p>
-            {tabs2
+            {tabs
                 .into_iter()
                 .map(|tab| {
                     view! {
                         <div class:hidden=move || {
                             active_tab.map(|active_tab| active_tab.get()).unwrap_or_default()
-                                != "Tab1".to_string()
-                        }>
-
-                            {tab}
-                        </div>
+                                != tab.id
+                        }>{tab.content}</div>
                     }
                 })
                 .collect_view()}
+
         </div>
     }
 }
 
 // TODO: Add tabs array default for const generic
-
-/*
-{tabs2
-                .into_iter()
-                .map(|tab| {
-                    view! {
-                        <div class:hidden=move || {
-                            active_tab.map(|active_tab| active_tab.get()).unwrap_or_default()
-                                != "Tab1".to_string()
-                        }>
-
-                            {tab}
-                        </div>
-                    }
-                })
-                .collect_view()}
-*/
 
