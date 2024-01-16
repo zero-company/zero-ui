6e42c4999a4e638c6cf52e79c84bf7520d8c83c5
Jigz <jigzpalillo@gmail.com>
2024-01-16 12:37:29 +0800

    [zero-ui] Update Tabs

--- rust/packages/zero-ui-leptos/src/lib/main-app/main-index-page.rs
+++ rust/packages/zero-ui-leptos/src/lib/main-app/main-index-page.rs
@@ -8,6 +8,7 @@ pub fn MainIndexPage() -> impl IntoView {
     view! {
         <div class="my-0 mx-auto max-w-3xl text-center">
             <h2 class="p-6 text-4xl">"Main Index Page"</h2>
+            <p class="p-6">"active_tab:" {move || { active_tab() }}</p>
             <button
                 class="bg-amber-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg"
                 on:click=move |_| {
@@ -15,9 +16,16 @@ pub fn MainIndexPage() -> impl IntoView {
                 }
             >
 
-                "active_tab: "
-                {move || { active_tab() }}
-                "--"
+                "Tab2"
+            </button>
+            <button
+                class="bg-amber-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg"
+                on:click=move |_| {
+                    set_active_tab.update(|active_tab| *active_tab = "Tab1".to_string())
+                }
+            >
+
+                "Tab1"
             </button>
 
             <Tabs tabs=[
